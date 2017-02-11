dc9832801b63959e77814f230300973c784535d7
Valter Lima Mesquita <limavalter@gmail.com>
2017-02-11 12:37:13 -0300

    Removed local ip param

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n');
-}).listen(port, '127.0.0.1');
+}).listen(port);
 
-console.log('Server running at http://127.0.0.1:' + port + '/');
+console.log('Server running at http://localhost:' + port + '/');
